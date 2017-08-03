07f1e0d3ac5f5b281140737ff3e646ade038b910
Anny Chang <AnnyChang@kingstono365.onmicrosoft.com>
2017-08-03 10:54:10 +0800

    Add Server Configurations in wdio.conf.js

--- wdio.conf.js
+++ wdio.conf.js
@@ -1,5 +1,20 @@
 exports.config = {
+    // CONFIGURATION FILE
+    // http://webdriver.io/guide/testrunner/configurationfile.html
     
+    // =====================
+    // Server Configurations
+    // =====================
+    // Host address of the running Selenium server. This information is usually obsolete as
+    // WebdriverIO automatically connects to localhost. Also if you are using one of the
+    // supported cloud services like Sauce Labs, Browserstack or Testing Bot you also don't
+    // need to define host and port information because WebdriverIO can figure that out
+    // according to your user and key information. However if you are using a private Selenium
+    // backend you should define the host address, port, and path here.
+    //
+    host: '0.0.0.0',
+    port: 4444,
+    path: '/wd/hub',
     //
     // ==================
     // Specify Test Files
@@ -54,8 +69,8 @@ exports.config = {
         // 5 instances get started at a time.
         maxInstances: 5,
 
-        // browserName: 'chrome',
-        browserName: 'internet explorer',
+        browserName: 'chrome',
+        // browserName: 'internet explorer',
     }],
     //
     // ===================
